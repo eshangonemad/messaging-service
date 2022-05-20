59aab4a6e46a8bdaa976a4969220feadcaee7126
eshangonemad <83403698+eshangonemad@users.noreply.github.com>
2022-05-20 14:41:57 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,10 +5,37 @@ radio.onReceivedString(function (receivedString) {
     if (receivedString == "a") {
         basic.showString("A")
     }
+    if (receivedString == "b") {
+        basic.showString("B")
+    }
 })
 input.onButtonPressed(Button.B, function () {
     if (letter == 1) {
         radio.sendString("a")
+        letter = 0
+    }
+    if (letter == 2) {
+        radio.sendString("b")
+        letter = 0
+    }
+    if (letter == 3) {
+        radio.sendString("c")
+        letter = 0
+    }
+    if (letter == 4) {
+        radio.sendString("d")
+        letter = 0
+    }
+    if (letter == 5) {
+        radio.sendString("e")
+        letter = 0
+    }
+    if (letter == 6) {
+        radio.sendString("f")
+        letter = 0
+    }
+    if (letter == 7) {
+        radio.sendString("g")
         letter = 0
     }
 })
